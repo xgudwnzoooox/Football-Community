02373cbbded6fe7a9e6afd93e42f410189c970cb
xgudwnzoooox <baehju2718@naver.com>
2022-05-04 01:12:46 +0900

    make file & folder

---
+++ src/fbase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDp1h1OtQbjsvorL0ctWRh-SSXrRkMv8Uo",
+  authDomain: "footballcomunity-c347a.firebaseapp.com",
+  projectId: "footballcomunity-c347a",
+  storageBucket: "footballcomunity-c347a.appspot.com",
+  messagingSenderId: "411739694373",
+  appId: "1:411739694373:web:b44dd39e12c077b5d166cc",
+  measurementId: "G-37L8BLLFTJ"
+};
+
+const app = initializeApp(firebaseConfig);
---
+++ src/routes/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+
+const Home = () => {
+
+  return (
+    <div></div>
+
+  );
+}
+
+export default Home;
